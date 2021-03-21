ddf98b2489860d25009460fb315c6071c9ec8018
jmpfbmx <jmpf.bmx@gmail.com>
2021-03-21 13:59:06 +0100

    suez: device: Label Prop overrides section

--- device.mk
+++ device.mk
@@ -148,10 +148,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(DEVICE)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-
 # EXFat
 WITH_EXFAT := true
 
+# PROP Overrides
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 # HIDL
