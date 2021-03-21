723d0e63aa9f787416abdcc5c590bddce29eb69f
jmpfbmx <jmpf.bmx@gmail.com>
2021-03-21 13:58:30 +0100

    suez: device: Label EXFat

--- device.mk
+++ device.mk
@@ -148,6 +148,8 @@ PRODUCT_COPY_FILES += \
     $(DEVICE)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(DEVICE)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+
+# EXFat
 WITH_EXFAT := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
