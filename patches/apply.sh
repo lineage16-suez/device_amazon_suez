e716a73554f86ac5cdae833e310740e1d8b3ca97
ggow <9898181+ggow@users.noreply.github.com>
2022-01-30 19:50:17 +0000

    patches: update to include frameworks/base folder

--- patches/apply.sh
+++ patches/apply.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="frameworks/av hardware/libhardware"
+dirs="frameworks/av frameworks/base hardware/libhardware"
 
 for dir in $dirs ; do
 	cd $rootdirectory
