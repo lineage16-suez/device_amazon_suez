4572dbcf050e2e171d303dd560bf74fdbe069c06
ggow <9898181+ggow@users.noreply.github.com>
2022-02-26 19:35:42 +0000

    manifest: uprev configstore to v1.1 and add missing package entry in hidl

--- hidl.mk
+++ hidl.mk
@@ -15,6 +15,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
+# Configstore
+    PRODUCT_PACKAGES += android.hardware.configstore@1.1-service
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
