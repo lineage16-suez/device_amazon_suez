712d346fe9cd25ea4a66f65a3f9afcdc8dd43eab
ggow <9898181+ggow@users.noreply.github.com>
2022-03-23 00:20:57 +0000

    build: include bromite-webview package

--- device.mk
+++ device.mk
@@ -73,6 +73,10 @@ PRODUCT_COPY_FILES += \
     $(DEVICE)/bluetooth/aptx/libaptX_encoder.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libaptX_encoder.so \
     $(DEVICE)/bluetooth/aptx/libaptXHD_encoder.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libaptXHD_encoder.so
 
+# Bromite Webview
+PRODUCT_PACKAGES += \
+    bromite-webview
+
 # Camera
 #PRODUCT_COPY_FILES += \
 
